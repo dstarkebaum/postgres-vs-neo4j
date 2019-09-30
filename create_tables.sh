7ba576806a60d0ba76dc34af363298751910e0fa
David Starkebaum <dstarkebaum@gmail.com>
2019-09-30 00:01:46 -0700

    remove primary keys from create_tables.sh and add timing tracker to load_tables.sh

--- create_tables.sh
+++ create_tables.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Create tables
-psql -c "CREATE TABLE papers (id VARCHAR(40) PRIMARY KEY, title TEXT, year SMALLINT, doi TEXT);"
-psql -c "CREATE TABLE inCits (id VARCHAR(40), inCit_id VARCHAR(40), PRIMARY KEY (id, inCit_id));"
+#psql -c "CREATE TABLE papers (id VARCHAR(40) PRIMARY KEY, title TEXT, year SMALLINT, doi TEXT);"
+#psql -c "CREATE TABLE inCits (id VARCHAR(40), inCit_id VARCHAR(40), PRIMARY KEY (id, inCit_id));"
+psql -c "CREATE TABLE papers (id VARCHAR(40), title TEXT, year SMALLINT, doi TEXT);"
+psql -c "CREATE TABLE inCits (id VARCHAR(40), inCit_id VARCHAR(40));"
 #id VARCHAR(40) REFERENCES papers(id),
 #inCit_id VARCHAR(40) REFERENCES papers(id),
-psql -c "CREATE TABLE outCits (id VARCHAR(40), outCit_id VARCHAR(40),PRIMARY KEY (id,outCit_id));"
+#psql -c "CREATE TABLE outCits (id VARCHAR(40), outCit_id VARCHAR(40),PRIMARY KEY (id,outCit_id));"
+psql -c "CREATE TABLE outCits (id VARCHAR(40), outCit_id VARCHAR(40));"
 #outCit_id VARCHAR(40) REFERENCES papers(id),
-psql -c "CREATE TABLE authors (ser SERIAL PRIMARY KEY, id INTEGER, name TEXT);"
-psql -c "CREATE TABLE paper_authors (ser SERIAL PRIMARY KEY, paper_id VARCHAR(40),author_id INTEGER);"
+#psql -c "CREATE TABLE authors (ser SERIAL PRIMARY KEY, id INTEGER, name TEXT);"
+#psql -c "CREATE TABLE paper_authors (ser SERIAL PRIMARY KEY, paper_id VARCHAR(40),author_id INTEGER);"
+psql -c "CREATE TABLE authors (id INTEGER, name TEXT);"
+psql -c "CREATE TABLE paper_authors (paper_id VARCHAR(40),author_id INTEGER);"
--- load_tables.sh
+++ load_tables.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-export server_host=10.0.0.9
+#export PSQL_HOST=10.0.0.9
+export start_time=$SECONDS
 #aws s3 cp s3://data-atsume-arxiv/open-corpus/2019-09-17/s2-corpus-000.gz data/s2-corpus/s2-corpus-000.gz
 #gunzip data/s2-corpus/s2-corpus-000.gz
 
 function make_csv {
-  echo "Downloading $1.gz from S3..."
+  echo "$((SECONDS-start_time)): Downloading $1.gz from S3..."
   aws s3 cp s3://data-atsume-arxiv/open-corpus/2019-09-17/$1.gz data/s2-corpus/$1.gz
-  echo "Unzipping $1.gz"
+  echo "$((SECONDS-start_time)): Unzipping $1.gz"
   gunzip -f data/s2-corpus/$1.gz
-  echo "Converting $1 JSON to CSV tables"
+  echo "$((SECONDS-start_time)): Converting $1 JSON to CSV tables"
   python3 parse_json_to_csv.py $1
 }
 
 function load_csv {
   #psql -h '10.0.0.5' -d ubuntu -U ubuntu -c \
   #psql -h 'localhost' -d david -U david -c "\copy ${1} (${2}) from $(pwd)/data/csv/${1}.csv with delimiter as '|'"
-  echo "Copying $(pwd)/data/csv/${1}.csv into $USER: ${1}(${2}) at $server_host"
-  psql -h "$server_host" -d $USER -U $USER -c "\copy ${1}(${2}) FROM $(pwd)/data/csv/${1}.csv WITH (FORMAT CSV, HEADER, DELIMITER '|')"
+  echo "$((SECONDS-start_time)): Copying $(pwd)/data/csv/${1}.csv into $USER: ${1}(${2}) at $PSQL_HOST"
+  psql -h "$PSQL_HOST" -d $USER -U $USER -c "\copy ${1}(${2}) FROM $(pwd)/data/csv/${1}.csv WITH (FORMAT CSV, HEADER, DELIMITER '|')"
   #echo "Deleting duplicates in the authors table based on authors.id"
   #psql -h 'localhost' -d $USER -U $USER -c "DELETE FROM authors a USING authors b WHERE a.ser < b.ser AND a.id = b.id"
   #echo "Deleting duplicates in the author_papers table"
@@ -26,7 +27,7 @@ function load_csv {
 }
 #users (id, email, first_name, last_name)
 
-for i in {001..001}
+for i in {001..176}
 do
   make_csv "s2-corpus-$i"
   load_csv "papers" "id, title, year, doi"
@@ -34,7 +35,7 @@ do
   load_csv "outCits" "id, outCit_id"
   load_csv "authors" "id, name"
   load_csv "paper_authors" "paper_id, author_id"
-  echo "clearing data folder"
+  echo "$((SECONDS-start_time)): clearing data folder"
   rm -r data/*
 done
 
