5b821d1e2ce6f7191201c9c355253bd8e0b8194b
David Starkebaum <dstarkebaum@gmail.com>
2019-10-02 23:37:12 -0700

    include aws sync

--- load_neo4j_nozip.sh
+++ load_neo4j_nozip.sh
@@ -5,17 +5,17 @@ export start_time=$SECONDS
 # neo4j settings can be adjusted here of needed:
 # sudo nano /etc/neo4j/neo4j.conf
 
-#aws s3 sync s3://data-atsume-arxiv/open-corpus/2019-09-17/neo4j data/neo4j
+aws s3 sync s3://data-atsume-arxiv/open-corpus/2019-09-17/neo4j data/neo4j
 
 # use glob to find all files like "data/neo4j/s2-corpus-001_papers.csv"
 # then store the results in a comma-separated string to pass to neo4j-admin
-papers="data/neo4j/papers_header.csv,`echo data/neo4j-unzipped/*papers.csv | tr ' ' ','`"
-cites="data/neo4j/cites_header.csv,`echo data/neo4j-unzipped/*cites.csv | tr ' ' ','`"
-is_cited_by="data/neo4j/is_cited_by_header.csv,`echo data/neo4j-unzipped/*is_cited_by.csv | tr ' ' ','`"
+papers="data/neo4j/papers_header.csv,`echo data/neo4j/*papers.csv | tr ' ' ','`"
+cites="data/neo4j/cites_header.csv,`echo data/neo4j/*cites.csv | tr ' ' ','`"
+is_cited_by="data/neo4j/is_cited_by_header.csv,`echo data/neo4j/*is_cited_by.csv | tr ' ' ','`"
 
-authors="data/neo4j/authors_header.csv,`echo data/neo4j-unzipped/*authors.csv | tr ' ' ','`"
-is_author_of="data/neo4j/is_author_of_header.csv,`echo data/neo4j-unzipped/*is_author_of.csv | tr ' ' ','`"
-has_author="data/neo4j/has_author_header.csv,`echo data/neo4j-unzipped/*has_author.csv | tr ' ' ','`"
+authors="data/neo4j/authors_header.csv,`echo data/neo4j/*authors.csv | tr ' ' ','`"
+is_author_of="data/neo4j/is_author_of_header.csv,`echo data/neo4j/*is_author_of.csv | tr ' ' ','`"
+has_author="data/neo4j/has_author_header.csv,`echo data/neo4j/*has_author.csv | tr ' ' ','`"
 
 #echo $papers
 #echo $cites
