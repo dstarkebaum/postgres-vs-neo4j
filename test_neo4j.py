c58e969947ad6e690ec9d105cd100dd40fcb1868
David Starkebaum <dstarkebaum@gmail.com>
2019-10-10 14:34:55 -0700

    make a separate testing script

--- test_neo4j.py
+++ test_neo4j.py
@@ -6,23 +6,14 @@ import setup.neo4j_utils as n4j
 # Be sure to clear out any previous database before you begin:
 # sudo rm -r /var/lib/neo4j/data/databases/graph.db
 
-
 pop.populate_database(
         corpus_path='data/s2-corpus',
         csv_path='data/csv',
         prefix='s2-corpus',
         suffix='.csv',
         start=0,
-        end=176,
+        end=0,
         compress=False,
         engine='neo4j',
-        testing=False,
+        testing=True,
         cache=True)
-
-
-#bucket='data-atsume-arxiv'
-#source='open-corpus/2019-09-17/s2-corpus-000.gz'
-#destination='data/s2-corpus/s2-corpus-000.gz'
-
-#s3 = boto3.client('s3')
-#s3.download_file(bucket, source, destination)
