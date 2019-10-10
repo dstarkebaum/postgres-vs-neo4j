3c876a8b4aad6a3174f52fb818faf7947baebaed
David Starkebaum <dstarkebaum@gmail.com>
2019-10-09 22:36:40 -0700

    completed process monitor to colect RAM stats

---
+++ process_monitor.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+import argparse
+import subprocess as sub
+from datetime import datetime
+import time
+
+def get_pids(process):
+    try:
+        output = sub.check_output(['pgrep','-f', process]).split(b'\n')
+        pidlist = [ x.decode('utf-8') for x in output]
+
+    except sub.CalledProcessError:
+        pidlist = []
+    #print 'list of PIDs = ' + ', '.join(str(e) for e in pidlist)
+    return pidlist
+
+
+
+def get_raw_mem(pid):
+    try:
+        #print(pid)
+        mem = sub.check_output(['sudo','ps_mem', '-p', pid, '-t']).decode('utf-8')
+    except sub.CalledProcessError:
+        mem = '-1'
+    return mem
+
+
+
+
+def monitor_process(process,output=''):
+    if len(output)==0:
+        output = '{p}_mem.log'.format(p=process)
+    quit=False
+    with open(output, 'a+') as f:
+        while not quit:
+
+            pids = get_pids(process)
+
+            if 0 == len(pids):
+                quit=True
+                print('Process '+ process + ' no longer exists!' )
+                return None
+
+            msg = datetime.now().strftime("%m/%d/%Y,%H:%M:%S") + \
+                    " "+pids[0]+" "+str(get_raw_mem(pids[0]))
+            print(msg)
+            f.write(msg)
+            time.sleep(2)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('process',type=str,
+            help='the name of the process to track')
+    parser.add_argument('-o','--output',type=str,default='',
+            help='filename of the log')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    #sub.Popen(['pgrep', args.process])
+        #sudo pgrep -f neo4j
+    monitor_process(args.process, args.output)
+
+if __name__ == "__main__":
+    main()
