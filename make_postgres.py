a8f110fcb0f537468181ce47d27cc10a6fce8cda
David Starkebaum <dstarkebaum@gmail.com>
2019-10-14 20:48:33 -0700

    make_postgres with 50M records

---
+++ make_postgres.py
@@ -0,0 +1,28 @@
+
+import setup.populate_database as pop
+import setup.postgres_utils as pgu
+
+pgu.main()
+pop.populate_database(
+        corpus_path='data/s2-corpus',
+        csv_path='data/csv',
+        prefix='s2-corpus',
+        suffix='.csv',
+        start=0,
+        end=50,
+        compress=False,
+        engine='psql',
+        make_int=True,
+        testing=False,
+        database='local'
+        )
+
+#pop.populate_neo4j()
+#pop.download_from_s3()
+
+#bucket='data-atsume-arxiv'
+#source='open-corpus/2019-09-17/s2-corpus-000.gz'
+#destination='data/s2-corpus/s2-corpus-000.gz'
+
+#s3 = boto3.client('s3')
+#s3.download_file(bucket, source, destination)
