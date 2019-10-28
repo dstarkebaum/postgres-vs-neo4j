6f3aa86cbd5022d1013e619e3f6228f721606c6a
David Starkebaum <dstarkebaum@gmail.com>
2019-10-27 23:33:20 -0700

    Clean up the memory-tracker script

--- postgres/track_memory.sh
+++ postgres/track_memory.sh
@@ -1,8 +1,7 @@
-while true; do ps -u neo4j -o pid -o %mem -o %cpu -o cputime -o etime -o command --width 200 >> postgres_ps.log; sleep 10; done
-ps_mem -p $(pgrep -d, postgres) -t -w 1 | while read line; do echo $(date +"%T") $line; done >> [PID]_mem.log
+#!/bin/bash
 
+# start a loop to record the %RAM and %CPU usage of posgres every 10 seconds
+while true; do ps -u postgres -o pid -o %mem -o %cpu -o cputime -o etime -o command --width 200 >> postgres_ps.log; sleep 10; done
 
-#nohup sudo ps_mem -p $(pgrep -d, postgres) -t -w 1 | while read line; do echo $(date +"%T") $line; done >> postgres_mem.log &
-
-#ps -o pid,user,%mem,command ax | grep postgres | sort -b -k3 -r
-# ps -o pid,user,%mem,command ax | grep postgres | sort -b -k3 -r
+# Record the exact RAM usage in bytes every 10 seconds
+ps_mem -p $(pgrep -d, postgres) -t -w 10 | while read line; do echo $(date +"%T") $line; done >> [PID]_mem.log
